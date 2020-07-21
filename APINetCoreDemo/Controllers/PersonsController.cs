68bb63500550ad23305f8dc8cb4e57044a63e324
Diego Moreira Vieira <diegovieira.ti@gmail.com>
2020-07-20 22:05:44 -0300

    Implementando os 4 verbos principais

--- APINetCoreDemo/Controllers/PersonsController.cs
+++ APINetCoreDemo/Controllers/PersonsController.cs
@@ -11,29 +11,36 @@ namespace APINetCoreDemo.Controllers
     [Route("[controller]")]
     public class PersonsController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<PersonsController> _logger;
-
-        public PersonsController(ILogger<PersonsController> logger)
-        {
-            _logger = logger;
+            return new string[] { "value1", "value2" };
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
