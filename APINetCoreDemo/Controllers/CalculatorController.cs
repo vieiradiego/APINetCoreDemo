591cf6597e5105a9f5bacd24c3887de2a4acbdd0
Diego Moreira Vieira <diegovieira.ti@gmail.com>
2020-07-20 21:02:34 -0300

    new file:   .gitignore 	new file:   APINetCoreDemo.sln 	new file:   APINetCoreDemo/APINetCoreDemo.csproj 	new file:   APINetCoreDemo/Controllers/WeatherForecastController.cs 	new file:   APINetCoreDemo/Controllers/CalculatorController.cs 	new file:   APINetCoreDemo/Program.cs 	new file:   APINetCoreDemo/Properties/launchSettings.json 	new file:   APINetCoreDemo/Startup.cs 	new file:   APINetCoreDemo/WeatherForecast.cs 	new file:   APINetCoreDemo/appsettings.Development.json 	new file:   APINetCoreDemo/appsettings.json

---
+++ APINetCoreDemo/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APINetCoreDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        //GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber));
+            }
+            return BadRequest ("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
