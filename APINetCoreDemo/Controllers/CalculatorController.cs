064c4acd9fc9be0139fa41be41ba694cf9956dd6
Diego Moreira Vieira <diegovieira.ti@gmail.com>
2020-07-20 21:37:09 -0300

    "Adicionado as funcionalidades API sum, subtraction, division, multiplication, mean e square-root"

--- APINetCoreDemo/Controllers/CalculatorController.cs
+++ APINetCoreDemo/Controllers/CalculatorController.cs
@@ -11,15 +11,76 @@ namespace APINetCoreDemo.Controllers
     [Route("[controller]")]
     public class CalculatorController : ControllerBase
     {
-        //GET api/values/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        //GET api/values/sum/5/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public ActionResult Sum(string firstNumber, string secondNumber)
         {
             if (isNumeric(firstNumber) && isNumeric(secondNumber))
             {
                 var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber));
+                return Ok(sum.ToString());
             }
             return BadRequest ("Invalid Input");
+        }
+
+        //GET api/values/subtraction/5/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var subtraction = (ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber));
+                return Ok(subtraction.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        //GET api/values/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public ActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var division = (ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber));
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        //GET api/values/multiplication/5/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public ActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var multiplication = (ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber));
+                return Ok(multiplication.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        //GET api/values/mean/5/5
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public ActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(mean.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        //GET api/values/square-root/5
+        [HttpGet("square-root/{number}")]
+        public ActionResult SquareRoot(string number)
+        {
+            if (isNumeric(number))
+            {
+                var squareRoot = Math.Sqrt((double)ConvertToDecimal(number));
+                return Ok(squareRoot.ToString());
+            }
+            return BadRequest("Invalid Input");
         }
 
         private decimal ConvertToDecimal(string number)
